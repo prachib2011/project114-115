cfc8732ddc6d572d0388f589f1d24b91276c3186
prachib2011 <89467902+prachib2011@users.noreply.github.com>
2022-02-04 16:07:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+mouthX=0;
+mouthY=0;
+
 function preload(){
+  lipstick = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png")
 }
 
 function setup(){
@@ -10,26 +14,29 @@ function setup(){
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
-     
-}
+     }
 
-function modelLoaded(){
-  console.log('PoseNet Is Initialized');
-}
+     function modelLoaded(){
+       console.log('PoseNet Is Initialized');
+     }
 
-function gotPoses(results){
-  if (results.length > 0)
-  {
-     console.log(results);
-     console.log("mouth x = " + results[0].pose.mouth.x);
-     console.log("mouth y = " + results[0].pose.mouth.y);
-  }
-}
+     function gotPoses(results){
+       if (results.length > 0)
+       {
+          console.log(results);
+          mouthX = results[0].pose.mouth.x;
+          mouthY = results[0].pose.mouth.y;
+          console.log("mouth x = " + mouthX);
+          console.log("mouth y = " + mouthY);
+       }
+     }
 
-function draw(){
-image(video, 0, 0, 300, 300)
+ function draw(){
+  image(video, 0, 0, 300, 300);
+  image(lipstick, mouthX, mouthY, 30, 30);
+  
 }
 
 function take_snapshot(){
-  save("funny.png");
+  save("filter.png");
 }
